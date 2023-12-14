727644bba269978b97c62758b44d9172ca6770d8
phianova <98988580+phianova@users.noreply.github.com>
2023-12-14 12:21:45 +0000

    character interaction, win conditions and audio

--- game.js
+++ game.js
@@ -35,11 +35,11 @@ class Character {
 
 }
 
-
 class Player extends Character {
     constructor(name, description, room, score) {
         super (name, description, room)
         this._score = score;
+        this._items = [];
     }
 
     set name(inputName) {
@@ -50,12 +50,20 @@ class Player extends Character {
         this._name = inputName;
     }
 
+    set items(value) {
+        this._items = value;
+    }
+
+    get items() {
+        return this._items;
+    }
 }
 
 class NPC extends Character {
     constructor(name, description, room) {
         super (name, description, room)
         this._dialogue = "";
+        this._items = "";
     }
 
     set dialogue(value) {
@@ -82,35 +90,80 @@ class NPC extends Character {
         return this._answers;
     }
 
+    set items(value) {
+        this._items = value;
+    }
+
+    get items() {
+        return this._items;
+    }
+
     generateDialogue (type) {
         let answerText = "";
         let dialogueText = this._dialogue[type];
         if (type === "question") {
-            console.log(this.answers)
-            let answers = this.answers
+            let answersKeys = Object.keys(this.answers);
+            let answersProperties = Object.values(this.answers);
+            let answersValues = []
+            for (let i = 0; i < answersProperties.length; i++) {
+                answersValues.push(answersProperties[i].answer);
+            }
             let answersArray = []
-            answerText = () => {
-                for (let i = 0; i < answers.length;i++){
-                    answersArray.push(Object.keys(answers)[i]);
-                    console.log(answersArray);
-                }
-                return "<br>" + answersArray.join(" <br> ");
+            for (let i = 0; i < answersKeys.length; i++) {
+                answersArray.push(answersKeys[i]);
+                answersArray.push(answersValues[i]);
+                answersArray.push("<br>")
+            }
+            answerText = "<br>" + answersArray.join(" ");
+        } else if (type === "correct") {
+            if (this._items !== "") {
+                let inventory = PlayerOne.items;
+                inventory.push(this._items[0]);
+                if (this._name === "yoga instructor") {
+                    answerText = `<br><br> You have proven you are not into yoga!`
+                } else if (this._name === "lady") {
+                    answerText = `<br><br> You have proven you like getting caught in the rain!`
+                } else {
+                    answerText = `<br><br> You have received a ${this._items[0]}!`;
+                }console.log(inventory);
             }
         }
-        return dialogueText + answerText();
+        return `The ${this._name} says: "${dialogueText}" ${answerText}`;
     }
     
-    converse() {
+    getDialogueType(answer) {
         let dialogueType = "";
         if (this._attempts === 0) {
             dialogueType = "question";
-        } else if (this._attempts === 1 && answerCorrect()) {
+        } else if (this._attempts === 1 && this.answerCorrect(answer)) {
             dialogueType = "correct";
-        } else if (this._attempts === 1 && !answerCorrect()) {
+        } else if (this._attempts === 1 && !this.answerCorrect(answer)) {
             dialogueType = "incorrect";
-        }     
+        } else if (this._attempts >= 2) {
+            dialogueType = "complete";
+        }
+        this.attempts ++;     
         return dialogueType;
     }
+
+    answerCorrect(answer) {
+        let answersKeys = Object.keys(this.answers);
+        let answersProperties = Object.values(this.answers);
+        let answersValues = []
+        for (let i = 0; i < answersProperties.length; i++) {
+            answersValues.push(answersProperties[i].correct);
+        }
+        let correct = false;
+        for (let i = 0; i < answersKeys.length; i++) {
+            if (answersKeys[i] === answer) {
+                if (answersValues[i] === true) {
+                    correct = true;
+                }
+            }
+        }
+        return correct;
+    }
+    
 }
 
 class Enemy extends NPC {
@@ -126,6 +179,8 @@ class Enemy extends NPC {
     get hitPoints() {
         return this._hitPoints;
     }
+
+
 
 }
 
@@ -146,14 +201,6 @@ class Room {
         this._description = value;
     }
 
-    set items(value) {
-        this._items = value;
-    }
-
-    get items() {
-        return this._items;
-    }
-
     get name() {
         return this._name;
     }
@@ -168,6 +215,10 @@ class Room {
 
     get character() {
         return this._character;
+    }
+
+    get linkedRoomList() {
+        return Object.entries(this._linkedRooms);
     }
 
     linkRoom(room, dir) {
@@ -194,10 +245,6 @@ class Room {
     } 
 
     moveRooms(direction) {
-        console.log(direction);
-        console.log(this);
-        console.log(this.linkedRoomsList);
-        console.log(this._linkedRooms[direction])
         return this._linkedRooms[direction];
     }
 
@@ -229,15 +276,29 @@ const OMalleys = new Room("bar, which is called O'Malley's", "It's a seedy kind 
 
 const PlayerOne = new Player("Your Name", "A player", "Beach", 0);
 
-const Bartender = new NPC("bartender", "shaking up some cocktails behind a beach bar. He looks busy, but is eyeing you up in case you want to order something.", "Beach");
+const Bartender = new NPC("bartender", "shaking up some cocktails behind a beach bar. He looks busy, but is eyeing you up in case you want to order something", "Beach");
 const Butcher = new NPC("butcher", "bringing a huge cleaver down onto a massive slab of flesh. He looks up and smiles a big, beaming smile at you", "ButcherShop");
 const Grocer = new NPC("grocer","hurriedly serving a queue of customers. She adjusts her glasses down her nose to check the numbers on the rickety old till as she wordlessly punches in prices", "Greengrocer");
+const YogaInstructor = new NPC("yoga instructor", "sitting in lotus position in the middle of a moss garden, burning incense in a holder by her feet. She senses you approaching", "YogaRetreat");
 
-const YogaInstructor = new Enemy("yoga instructor", "sitting in lotus position in the middle of a moss garden, burning incense in a holder by her feet. She senses you approaching", "YogaRetreat", "dialogue", 5);
+//const YogaInstructor = new Enemy("yoga instructor", "sitting in lotus position in the middle of a moss garden, burning incense in a holder by her feet. She senses you approaching", "YogaRetreat", "dialogue", 5);
 const Lady = new Enemy("lady", "sitting cross-legged on one of the barstools, looking impatiently at the door. Wait! It's YOUR lady!", "OMalleys","dialogue", 5);
 
 Bartender.dialogue = bartenderDialogue;
 Bartender.answers = bartenderAnswers;
+Bartender.items = ["Pina colada"];
+YogaInstructor.dialogue = yogaDialogue;
+YogaInstructor.answers = yogaAnswers;
+YogaInstructor.items = ["Not into yoga"];
+Butcher.dialogue = butcherDialogue;
+Butcher.answers = butcherAnswers;
+Butcher.items = ["Half a brain"];
+Grocer.dialogue = grocerDialogue;
+Grocer.answers = grocerAnswers;
+Grocer.items = ["Champagne"];
+Lady.dialogue = ladyDialogue;
+Lady.answers = ladyAnswers;
+Lady.items = ["Caught in the rain"];
 
 
 Beach.linkRoom(Street, "East");
@@ -266,34 +327,33 @@ OMalleys.character = Lady;
 document.addEventListener("keydown", function (event) {
     action = "";
     const directions = ["north", "south", "east", "west"];
-    const items = ["quinoa", "champagne", "brain", "kidneys", "heart", "liver"]
     const answers = ["a", "b", "c", "d"]
-    const validCommands = ["start", "buy", "talk", "move", "answer", "north", "south", "east", "west"];
+    const validCommands = ["start", "restart", "talk", "move", "answer", "leave", "check", "north", "south", "east", "west"]; //for later: "buy",
     if (event.key === "Enter") {
         action = document.getElementById("input").value;
         actionWords = action.split(" ");
-        if (actionWords[0].toLowerCase() === "start") {
+        if (actionWords[0].toLowerCase() === "start" || actionWords[0].toLowerCase() === "restart") {
             startGame();
         } else if (!validCommands.includes(actionWords[0].toLowerCase())){
             alert("This is not a valid action. Please try again.");
         }
         if (actionWords[0].toLowerCase() === "move") {
-            if (directions.includes(actionWords[1].toLowerCase())) {
-                //TODO insert check to make sure direction goes somewhere
-                let capsDirection = capitalLetter(actionWords[1].toLowerCase());
-                let currentRoom = Player.room;
+            let currentRoom = PlayerOne.room;
+            let availableDirections = currentRoom.linkedRoomList.map(([direction]) => direction);
+            let capsDirection = capitalLetter(actionWords[1]);
+            if (directions.includes(actionWords[1].toLowerCase()) && availableDirections.includes(capsDirection)) {
                 let nextRoom = currentRoom.moveRooms(capsDirection);
                 newRoom(nextRoom);
             } else {
                 alert("This is not a valid direction to move in. Please try again.")
             }
+        
         }
-
         if (actionWords[0].toLowerCase() === "talk") {
-            let room = Player.room;
-            if (room.character !== "") {
-                let character = room.character;
-                let dialogueType = character.converse();
+            let currentRoom = PlayerOne.room;
+            if (currentRoom.character !== "") {
+                let character = currentRoom.character;
+                let dialogueType = character.getDialogueType();
                 let dialogue = character.generateDialogue(dialogueType);
                 newDialogueScreen(dialogue, dialogueType);
             } else {
@@ -303,20 +363,34 @@ document.addEventListener("keydown", function (event) {
 
         if (actionWords[0].toLowerCase() === "answer") {
             if (answers.includes(actionWords[1].toLowerCase())) {
-                //answerQuestion();
+                let currentRoom = PlayerOne.room;
+                let character = currentRoom.character;
+                let dialogueType = character.getDialogueType(actionWords[1].toLowerCase());
+                let dialogue = character.generateDialogue(dialogueType);
+                newDialogueScreen(dialogue, dialogueType);
             } else {
                 alert("This is not a valid answer. Please answer a, b, c or d.")
             }
         }    
 
-        /* FOR LATER */
-        if (actionWords[0].toLowerCase() === "buy") {
-            if (items.includes(actionWords[1].toLowerCase())) {
-                //buyItem();
+        if (actionWords[0].toLowerCase() === "leave") {
+            if (document.getElementById("dialogue-text").style.display != "none") {
+                endDialogue();
             } else {
-                alert("This is not a valid item to buy. Please try again.")
+                alert("You can't just 'leave' right now!")
             }
-        }
+        }    
+
+        if (actionWords[0].toLowerCase() === "check") {
+            let currentRoom = PlayerOne.room;
+            if (currentRoom.character === Lady && Lady.attempts !== 0) {
+                bossFight();
+            } else if (Lady.attempts === 0) {
+                alert("You need to answer your lady's question first.")
+            } else {
+                alert("You have nothing to check in this room.")
+            }        
+        }    
 
         //reset input regardless of value entered
         document.getElementById("input").value = "";
@@ -325,38 +399,99 @@ document.addEventListener("keydown", function (event) {
 })
 
 const startGame = () => {
+    var music = document.createElement("audio");
+    music.setAttribute("src", "./escape.mp3");
+    music.setAttribute("autoplay", "autoplay");
+    music.loop = true;
+
+    document.getElementById("room-name").innerHTML = capitalLetter(Beach.name);
     document.getElementById("room-text").innerHTML = Beach.roomMessage();
     document.getElementById("instructions").innerHTML = Beach.generateInstructions();
     document.getElementById("input").value = "";
-    Player.room = Beach;
+
+    PlayerOne.items = [];
+    Character.attempts = 0;
+
+    PlayerOne.room = Beach;
+    let roomCharacter = Beach.character;
+    roomCharacter.attempts = 0;
 }
 
 const newRoom = (room) => {
+    document.getElementById("room-name").innerHTML = capitalLetter(room.name);
+    document.getElementById("dialogue-text").style.display = "none";
     document.getElementById("room-text").innerHTML = room.roomMessage();
     document.getElementById("instructions").innerHTML = room.generateInstructions();
     document.getElementById("input").value = "";
-    Player.room = room;
+    PlayerOne.room = room;
     let roomCharacter = room.character;
-    roomCharacter.attempts = 0;
+    if (roomCharacter.attempts === undefined) {
+        roomCharacter.attempts = 0;
+    }
 }
 
 const newDialogueScreen = (dialogue, dialogueType) => {
     document.getElementById("dialogue-text").style.display = "flex";
     document.getElementById("dialogue-text").innerHTML = dialogue;
+    let currentRoom = PlayerOne.room;
+    let character = currentRoom.character;
     if (dialogueType === "question") {
-        document.getElementById("instructions").innerHTML = 'Type "answer" followed by your answer to proceed';
+        document.getElementById("instructions").innerHTML = 'Type "answer" followed by your answer (a, b, c or d) to proceed';
+    } else if (character === Lady) {
+        document.getElementById("instructions").innerHTML = 'Enter "check" to see if you have everything she\'s looking for - or "leave" to explore further first.';
     } else {
-        document.getElementById("instructions").innerHTML = "They don't seem to want to talk any more.";
+        document.getElementById("instructions").innerHTML = "They don't seem to want to talk any more. Type 'leave' to go back to looking around the room.";
     }
     document.getElementById("input").value = "";
 }
 
-/*
-const buyItem = () => {
-
+const endDialogue = () => {
+    document.getElementById("dialogue-text").style.display = "none";
+    newRoom(PlayerOne.room);
 }
-*/
 
+const bossFight = () => {
+    if (winConditions() === true) {
+        //win screen:
+        document.getElementById("room-name").innerHTML = "Well done!";
+        document.getElementById("dialogue-text").style.display = "flex";
+        document.getElementById("dialogue-text").innerHTML =  generateBossDialogue();
+        document.getElementById("room-text").style.display = "none";
+        document.getElementById("instructions").innerHTML = 'Enter "restart" to play again';
+        document.getElementById("input").value = "";
+    } else {
+        //lose screen:
+        document.getElementById("room-name").innerHTML = "Game over...";
+        document.getElementById("dialogue-text").style.display = "flex";
+        document.getElementById("dialogue-text").innerHTML = generateBossDialogue();
+        document.getElementById("room-text").style.display = "none";
+        document.getElementById("instructions").innerHTML = 'Enter "restart" to play again';
+        document.getElementById("input").value = "";   
+    }
+}
+
+const winItems = ["Pina colada", "Half a brain", "Not into yoga", "Champagne", "Caught in the rain"]
+
+const winConditions = () => {
+    let inventory = PlayerOne.items;
+    for (let i = 0; i<winItems.length; i++) {
+        if (!inventory.includes(winItems[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+const generateBossDialogue = () => {
+    let inventory = PlayerOne.items;
+    let string1 = (inventory.includes("Pina colada")) ? "You like pina coladas," : "You don't like pina coladas!";
+    let string2 = (inventory.includes("Caught in the rain")) ? "You like getting caught in the rain," : "You don't like getting caught in the rain...";
+    let string3 = (inventory.includes("Not into yoga")) ? "You're not into yoga," : "You're into yoga!";
+    let string4 = (inventory.includes("Half a brain")) ? "You have half a brain," : "You don't have half a brain!";
+    let string5 = (inventory.includes("Champagne")) ? "And you're into champagne!" : "But you're into health food, not champagne. Ew.";
+    let string6 = winConditions() ? "You have everything I'm looking for! Let's get out of here and escape." : "You don't have everything I'm looking for. Sorry.";
+    return `Your lady says: "${string1} <br> ${string2} <br> ${string3} <br> ${string4} <br> ${string5} <br> ${string6}`
+}
 
 //Functions to display or hide instructions.
 const displayHelp = () => {
